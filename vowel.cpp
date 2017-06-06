25418d82643e854a6c053a3d4d88075146a49444
puppy2102 <pupharita@gmail.com>
2017-06-06 14:36:03 +0530

    Add files via upload

---
+++ vowel.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+char ch;
+cout<<"enter the character";
+cin>>ch;
+if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+cout<<"vowels";
+else
+cout<<"consonants";
+}
