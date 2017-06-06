ffe667b6186f3e68724fd82b8569a2b052a7d03b
puppy2102 <pupharita@gmail.com>
2017-06-06 14:25:22 +0530

    Add files via upload

---
+++ +ve.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the no";
+cin>>n;
+if(n<0)
+    cout<<"negative";
+else
+cout<<"positive";
+getch();
+}
