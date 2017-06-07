f48e1353a5ea691ec8331191cbc24098034990fa
puppy2102 <pupharita@gmail.com>
2017-06-07 09:46:44 +0530

    Add files via upload

---
+++ natural.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int num,i,sum=0;
+cout<<"enter the no";
+cin>>num;
+for(i=0;i<=num;i++)
+{
+    sum=sum+i;
+
+}
+cout<<"sum is "<<sum;
+}
