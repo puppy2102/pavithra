cb40d7e3fd796d63e2ac225e54f597e44abadcbd
puppy2102 <pupharita@gmail.com>
2017-06-13 09:18:41 +0530

    Add files via upload

--- Large.java
+++ Large.java
@@ -1,10 +1,27 @@
 package pups;
 
+import java.util.Scanner;
+
 public class Large {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if((a>b)&&(a>c)){
+			System.out.println("largest is"+a);
+		}
+		else if((b>a)&&(b>c)){
+			System.out.println("largest is"+b);
+		}
+		else if((c>a)&&(c>b)){
+			System.out.println("largest is"+c);
+		}
+			}
 
+		
 	}
 
-}
+
