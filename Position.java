d05056498769ed4d39646d2192b9378900f59b94
puppy2102 <pupharita@gmail.com>
2017-06-14 21:48:47 +0530

    Add files via upload

---
+++ Position.java
@@ -0,0 +1,22 @@
+package Guvi;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Position {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("size");
+
+int b=sc.nextInt();
+int[]x=new int[50];
+for(int i=0;i<50;i++){
+	x[i]=sc.nextInt();
+	}
+System.out.println("position");
+Arrays.sort(x);
+
+		System.out.println(x[b]);
+}
+
+}
