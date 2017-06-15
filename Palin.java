d2a71b392894a6f35180aa9f51d58aec78d6b6d8
puppy2102 <pupharita@gmail.com>
2017-06-15 21:26:25 +0530

    Add files via upload

--- Palin.java
+++ Palin.java
@@ -1,32 +1,17 @@
-package pups;
-
+package Guvi;
 import java.util.Scanner;
-
 public class Palin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		int reverse=0;
-		int n1=num;
-		while(num!=0){
-			
-			reverse=reverse*10;
-					reverse=reverse+num%10;
-			num=num/10;
-			
+		String ori=sc.next();;
+		String rev="";
+		int a=ori.length();
+		for(int i=a-1;i>=0;i--){
+			rev=rev+ori.charAt(i);
 		}
-		if(n1==reverse){
-			System.out.println("it is palindrome");
-		}
-		else{
-			System.out.println("its is not a palindrome");
-		}
+		System.out.println(rev);
+	}
 
-			}
-
-		}
-
-	
-
+}
