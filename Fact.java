382bf243dc4f50eba767281dd125b27bf299a779
puppy2102 <pupharita@gmail.com>
2017-06-15 10:34:24 +0530

    Add files via upload

--- Fact.java
+++ Fact.java
@@ -1,17 +1,17 @@
-package pups;
+package Guvi;
 import java.util.Scanner;
 public class Fact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int fact=1;
-for(int i=1;i<=a;i++){
-	 fact=fact*i;
-	
-}
-System.out.println("factorial is"+fact);
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int fact=1;
+		for(int i=1;i<=a;i++){
+			fact=fact*i;
+		}
+		System.out.println("fact is"+fact);
+
 	}
 
 }
