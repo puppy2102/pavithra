dcf1b24df8ec2d1d933c47e377a5e3e987c6ff70
puppy2102 <pupharita@gmail.com>
2017-06-19 14:47:43 +0530

    Add files via upload

--- Primerange.java
+++ Primerange.java
@@ -1,9 +1,32 @@
 package Guvi;
-
+import java.util.Scanner;
 public class Primerange {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	
+				int count=0;
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+			
+				for(int i=a;i<=b;i++){
+			count=0;
+					
+					for( int n=1;n<=i;n++){
+						if(i%n==0){
+							count++;
+						}
+						if(count==2){
+							System.out.println(i);
+							break;
+
+			}
+
+				}
+			}
+			}
+		}
 
 	}
 
