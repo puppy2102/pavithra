9cbaee6a408ce9b73325e4cb5393fae9117d8b08
puppy2102 <pupharita@gmail.com>
2017-06-19 15:00:34 +0530

    Add files via upload

---
+++ Mins.java
@@ -0,0 +1,22 @@
+package Guvi;
+import java.util.Scanner;
+public class Mins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int  b=sc.nextInt();
+		
+			int c=a%100;
+			
+		
+		
+			int d=b%100;
+		
+		
+		int e=d-c;
+
+	}
+
+}
