2f97aefda048797e4a4923de9788abdc1b1b7842
puppy2102 <pupharita@gmail.com>
2017-07-04 10:06:02 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,18 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class Count {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int count=0;
+		int b=(int)Math.log10(a);
+		
+		System.out.println(b+1);
+	}
+	}
+
+
