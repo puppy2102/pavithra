339cadc58237ef9d3d138060b1345af5791d9b04
puppy2102 <pupharita@gmail.com>
2017-07-04 10:43:24 +0530

    Add files via upload

--- ReverseStri.java
+++ ReverseStri.java
@@ -1,10 +1,15 @@
 package Player;
 
+import java.util.Scanner;
+
 public class ReverseStri {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String ori=sc.next();
+		String reverse = new StringBuffer(ori).reverse().toString();
+		System.out.println("reverse is "+reverse);
 	}
 
 }
