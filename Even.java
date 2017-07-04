074bdee44f595109d7918096102029eed56c8750
puppy2102 <pupharita@gmail.com>
2017-07-04 09:20:19 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,19 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if((a%2)==0){
+			System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+
+}
