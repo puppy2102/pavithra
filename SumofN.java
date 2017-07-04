e466e79f2d7881d3df5c97566d711a2120795ef9
puppy2102 <pupharita@gmail.com>
2017-07-04 09:49:06 +0530

    Add files via upload

---
+++ SumofN.java
@@ -0,0 +1,18 @@
+package Beginner;
+
+import java.util.Scanner;
+
+public class SumofN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=a;i++){
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+
+}
