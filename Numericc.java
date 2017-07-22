2c17dbb8d38d45e5664c5d62ecc937f93dc91d80
puppy2102 <pupharita@gmail.com>
2017-07-22 10:39:00 +0530

    Add files via upload

--- Numericc.java
+++ Numericc.java
@@ -1,10 +1,26 @@
 package Pro;
 
+import java.util.Scanner;
+
 public class Numericc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s,p="";
+		Scanner sc=new Scanner(System.in);
+		s=sc.next();
+		char ch[]=s.toCharArray();
+		for(int i=0;i<s.length();i++){
+		if(ch[i]=='0'||ch[i]=='1'||ch[i]=='2'||ch[i]=='3'||ch[i]=='4'||ch[i]=='5'||ch[i]=='6'||ch[i]=='7'||ch[i]=='8'||ch[i]=='9'||ch[i]=='.'||ch[i]=='e'){
+		System.out.println("True");
+		break;
+		}
+		else{
+			System.out.println("False");
+		}
+		}
 	}
+	
+	
 
 }
