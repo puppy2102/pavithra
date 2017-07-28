dd1d6e3c8c10b26215e470dafa2196588930fc0d
puppy2102 <pupharita@gmail.com>
2017-07-28 14:07:54 +0530

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package SkillRack;
+
+import java.util.Scanner;
+
+public class Day5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+String a=sc.nextLine();
+String s[]=a.split(" ");
+for(int i=s.length;i>0;i--){
+	System.out.println(s[i]+" ");
+}
+	}
+
+}
