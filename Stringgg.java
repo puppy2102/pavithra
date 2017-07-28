4b1e05652ff17ef9d3881ae6377d4ca27596090a
puppy2102 <pupharita@gmail.com>
2017-07-28 12:29:27 +0530

    Add files via upload

--- Stringgg.java
+++ Stringgg.java
@@ -1,10 +1,22 @@
 package Player;
 
+import java.util.Scanner;
+
 public class Stringgg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		 String str=sc.next();
+	        char[] ch=str.toCharArray();
+	        int j=str.length();
+	        for(int i=0;i<j;i++)
+	        {
+	            if(ch[i]!=ch[j-1])
+	            {
+	                System.out.print(ch[i]+" "+ch[(j-1)-i]+" "); 
+	            }
+	        }
 	}
 
 }
